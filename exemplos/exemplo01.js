90df6111024ed178d6751fdaedc62c97cb426205
AlexandreProvensi <alexandreprovensi12@gmail.com>
2020-08-10 20:20:33 -0300

    Aula 10/08/2020

--- exemplos/exemplo01.js
+++ exemplos/exemplo01.js
@@ -4,13 +4,13 @@ const express = require('express');
 //Instanciando a aplicação
 const app = express();
 
-//Adicionando uma requisição para a rota raiz ('/') da aplicação
+//Adicionando uma requisição para a rota raíz ('/') da aplicação
 app.get('/', (req, res) => {
-    res.send('<h1>Its Work!</h1>')
+    res.send('<h1>Exemplo de retorno de uma lista e de um item  específico da lista</h1>');
 });
 
 //Nova rota
-app.get('/listaDeCompras', (req, res) => {
+app.get('/listaDeCompra', (req, res) => {
 
     var lista = '<ol>'
     lista += '<li>Pão</li>';
@@ -19,17 +19,18 @@ app.get('/listaDeCompras', (req, res) => {
     lista += '</ol>';
 
     res.send(lista);
-})
 
-//Nova rota com parâmetro
-app.get('/listaDeCompras/:item', (req, res) => {
+});
 
-    res.send('Você está solicitando o item:' + req.params.item);
+//Nova rota com parametro
+app.get('/listaDeCompra/:item', (req, res) => {
+
+    res.send('Você está solicitando o item: ' + req.params.item);
 
 })
 
 
 //Iniciando a aplicação na porta 3000
 app.listen(3000, () => {
-    console.log('Rodando na porta 3000')
+    console.log('Rodando na porta 3000');
 });
